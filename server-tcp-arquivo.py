444964654da375e102c25a21163dfb0299d6b27b
Jhonatan Gratieri Simoni <61829658+jhonegratieri@users.noreply.github.com>
2023-07-06 11:20:46 -0300

    Create server-tcp-arquivo.py

---
+++ server-tcp-arquivo.py
@@ -0,0 +1,21 @@
+import socket
+
+HOST = ''  # Endereco IP do Servidor
+PORT = 5000  # Porta que o Servidor esta
+
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+orig = (HOST, PORT)
+tcp.bind(orig)
+tcp.listen(1)
+
+while True:
+    con, cliente = tcp.accept()
+    print('Concetado por', cliente)
+    with open('arquivo-recebido', 'wb') as arquivo:
+        while True:
+            dados = con.recv(1024)
+            if not dados:
+                break
+            arquivo.write(dados)
+    print('Salvo com sucesso!')
+    con.close()
